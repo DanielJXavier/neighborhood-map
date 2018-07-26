8a46208ca4adc9051ae463b57b54a3505e59aad9
Daniel Xavier <dxdanielxavier@gmail.com>
2018-07-25 21:52:50 -0300

    Add places markers

--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react'
-import { withGoogleMap, GoogleMap } from 'react-google-maps'
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
 class Map extends Component {
 
   render() {
+    const places = [
+      {
+        name: 'Rancho Da Picanha',
+        address: '945, Av. Monte Verde, Camanducaia - MG, 37650-000',
+        lat: -22.862641,
+        lng: -46.035619
+      },
+      {
+        name: 'Mon\'t Burger',
+        address: 'Av. da Fazenda, 140 - Monte Verde, Camanducaia - MG, 37650-000',
+        lat: -22.865489,
+        lng: -46.043515
+      },
+      {
+        name: 'Alpminas',
+        address: 'Av. Monte Verde, 7, Camanducaia - MG, 37650-000',
+        lat: -22.862641,
+        lng: -46.036485
+      },
+      {
+        name: 'Villa Amarela',
+        address: 'Av. da Fazenda, 10 - Monte Verde, Camanducaia - MG, 37653-000',
+        lat: -22.864995,
+        lng: -46.043343
+      },
+      {
+        name: 'Casa da Pizza',
+        address: 'R. Pau Brasil, 309 - Monte Verde, Camanducaia - MG, 37650-000',
+        lat: -22.857085,
+        lng: -46.026858
+      }
+    ]
+
     const MonteVerdeMap = withGoogleMap(props => (
-      <GoogleMap defaultCenter={{ lat: -22.8632259, lng: -46.0392505 }} defaultZoom={ 15 } />
+      <GoogleMap defaultCenter={{ lat: -22.8632259, lng: -46.0392505 }} defaultZoom={ 15 }>
+        {places.map(place =>
+          <Marker position={{ lat: place.lat, lng: place.lng }} />
+        )}
+      </GoogleMap>
+
     ))
 
     return (
